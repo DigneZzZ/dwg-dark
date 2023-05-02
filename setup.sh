42c88f52a0f91aeb8b8939eaed2e74606ed78da0
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-05-02 19:31:26 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,27 +12,27 @@ apt install git -y
 printf "\e[42mGit успешно установлен.\e[0m\n"
 
 # Клонирование репозитория
-printf "\e[42mКлонирование репозитория dwg-ui...\e[0m\n"
-git clone https://github.com/dignezzz/dwg-ui.git temp
+printf "\e[42mКлонирование репозитория dwg-dark...\e[0m\n"
+git clone https://github.com/dignezzz/dwg-dark.git temp
 
-if [ ! -d "dwg-ui" ]; then
-  mkdir dwg-ui
-  echo "Папка dwg-ui создана."
+if [ ! -d "dwg-dark" ]; then
+  mkdir dwg-dark
+  echo "Папка dwg-dark создана."
 else
-  echo "Папка dwg-ui уже существует."
+  echo "Папка dwg-dark уже существует."
 fi
 
 # копирование содержимого временной директории в целевую директорию с перезаписью существующих файлов и папок
-cp -rf temp/* dwg-ui/
+cp -rf temp/* dwg-dark/
 
 # удаление временной директории со всем ее содержимым
 rm -rf temp
-printf "\e[42mРепозиторий dwg-ui успешно клонирован до актуальной версии в репозитории.\e[0m\n"
+printf "\e[42mРепозиторий dwg-dark успешно клонирован до актуальной версии в репозитории.\e[0m\n"
 
 # Переходим в папку ad-wireguard
-printf "\e[42mПереходим в папку dwg-ui...\e[0m\n"
-cd dwg-ui
-printf "\e[42mПерешли в папку dwg-ui\e[0m\n"
+printf "\e[42mПереходим в папку ddwg-dark...\e[0m\n"
+cd dwg-dark
+printf "\e[42mПерешли в папку dwg-dark\e[0m\n"
 
 # Установка прав на файл установки
 printf "\e[42mУстановка прав на файл установки...\e[0m\n"
@@ -70,5 +70,5 @@ printf "\e[48;5;202m\e[30m Адрес входа в веб-интерфейс Wi
 printf '\e[48;5;202m\e[30m ################################################################## \e[0m\n'
 printf '\e[48;5;202m\e[30m Не забудь отдельно установить UFW-Docker, для закрытия веб-интерфейса wireguard. \e[0m\n'
 printf '\e[48;5;196m\e[97m ВНИМАНИЕ! Запускать только после того как создадите для себя клиента в WireGUARD!!! \e[0m\n'
-printf '\e[48;5;202m\e[30m команда для установки: ./dwg-ui/tools/ufw-docker.sh \e[0m\n'
+printf '\e[48;5;202m\e[30m команда для установки: ./dwg-dark/tools/ufw-docker.sh \e[0m\n'
 printf '\e[48;5;202m\e[30m ################################################################## \e[0m\n'
